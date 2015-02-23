94b7b1d787909dfe092b95a353a7708791ed6ac5
Scott McCoy <scotthmccoy@gmail.com>
2015-02-22 17:01:16 -0800

    Initial Commit

---
+++ JoggCodingChallenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JoggCodingChallenge
+//
+//  Created by Scott McCoy on 2/22/15.
+//  Copyright (c) 2015 ScottSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
